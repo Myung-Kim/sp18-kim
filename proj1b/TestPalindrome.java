7137c49db2bf65b50afc0f8ff5359cefdb05bfb3
　金明毅 <myungil@jinmingyideMacBook-Pro.local>
2021-10-04 22:11:01 +0200

    finnished easily

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,8 +1,9 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +15,33 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+    // Uncomment this class once you've created your Palindrome class.
+
+    @Test
+    public void TestIsPalindrome() {
+        assertTrue(palindrome.isPalindrome("aka"));
+        assertTrue(palindrome.isPalindrome(""));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertFalse(palindrome.isPalindrome("kaa"));
+    }
+
+    @Test
+    public void TestIsPalindromeByOne() {
+//        OffByOne obo = new OffByOne();
+        // why?
+        CharacterComparator obo = new OffByOne();
+        assertTrue(palindrome.isPalindrome("axb", obo));
+        assertTrue(palindrome.isPalindrome("axyb", obo));
+        assertFalse(palindrome.isPalindrome("axa", obo));
+
+    }
+
+    @Test
+    public void TestIsPalindromeByN(){
+        CharacterComparator obo = new OffByN(2);
+        assertTrue(palindrome.isPalindrome("axc", obo));
+        assertTrue(palindrome.isPalindrome("bxzd", obo));
+        assertFalse(palindrome.isPalindrome("axa", obo));
+    }
 }
